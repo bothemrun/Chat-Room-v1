e783b7b17b13d772b97e85f6eb080d3119ea160a
bothemrun <b05902133@ntu.edu.tw>
2023-07-08 16:27:37 +0800

    docs: add explanation comments

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//load the socket.io-client, which exposes an io global (and the endpoint GET /socket.io/socket.io.js), and then connect.
+//https://socket.io/get-started/chat
+//Notice that I’m not specifying any URL when I call io(), since it defaults to trying to connect to the host that serves the page.
 const socket = io();
 
 const messages = document.getElementById("messages");
@@ -24,13 +27,37 @@ function call_send_message_api(){
 		console.log("user input in if:" + input.value);
 
 		//socket.emit("chat message", input.value);
+
+		//use ajax to call RESTful API
+		//send HTTP method(GET POST) requests to server.
+		//https://www.w3schools.com/js/js_ajax_intro.asp
+		//by an XMLHttpRequst object.
 		const xhttp = new XMLHttpRequest();
+
+		//callback when the request is received.
 		xhttp.onload = function(){
 			console.log("client successfully sent a new message to server with status code:" + this.status);
 			console.log(", with return responseText: " + this.responseText);
 		};
+
+		//HTTP POST
 		xhttp.open("POST", "/messages");
+
+		//specify http message body's Content-Type in header.
+		/*
+		in server.js:
+			//mounts express.js builtin middleware in Express that parses incoming requests with JSON payloads.
+			//Returns middleware that only parses JSON and only looks at requests where the Content-Type header matches the type option.
+			//a new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body).
+			//https://expressjs.com/en/api.html#express.json
+			app.use(express.json());
+		*/
 		xhttp.setRequestHeader("Content-Type", "application/json");
+
+		//Javascript JSON object to JSON strings.
+		//JSON is Javascript standard built-in objects like Javascript BigInt String.
+		//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+		//send(string) for HTTP POST method.
 		xhttp.send(JSON.stringify({"message":input.value}));
 		
 		//TODO: will nonblocking I/O causes the messages cleared before ajax sent?
@@ -38,10 +65,14 @@ function call_send_message_api(){
 	}
 };
 
+//socket.io broadcasting received.
 socket.on("chat message", function(new_msg){
+	//add a new entry <li> to a list <ul>
 	const new_msg_li = document.createElement("li");
 	new_msg_li.textContent = new_msg;
 	messages.appendChild(new_msg_li);
+
+	//chats scrolled down to the latest.
 	window.scrollTo(0, document.body.scrollHeight);
 });
 
--- server.js
+++ server.js
@@ -1,23 +1,54 @@
 const express = require("express");
-const app = require("express")();
-const http = require("http").Server(app);
+
+//top-level Express function / an Express server instance.
+//initialize a function handler supplied to an HTTP server.
+const app = require("express")();//TODO: `express()`
+
+//node.js http module's Server class.
+//specify the requestListener function.
+//https://nodejs.org/api/http.html#httpcreateserveroptions-requestlistener
+const http = require("http").Server(app);//TODO: use socket.io example: createServer()
+
+//initialize a new instance of socket.io by the HTTP server object.
+//https://socket.io/get-started/chat
 const io = require("socket.io")(http);
 const port = 3000
 
+//Sets the execution mode to verbose to produce long stack traces.
+//https://github.com/TryGhost/node-sqlite3/wiki/API
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("message_db.db");
 
-app.use(express.static("."));
+//mounts middleware function in Express that allow express server instance to serve files under /public.
+//https://expressjs.com/en/api.html#express.static
+app.use(express.static("/"));//TODO: /public
+
+//mounts express.js builtin middleware in Express that parses incoming requests with JSON payloads.
+//Returns middleware that only parses JSON and only looks at requests where the Content-Type header matches the type option. 
+//a new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body).
+//https://expressjs.com/en/api.html#express.json
 app.use(express.json());
 
+
+
+//Express Routing guide:
+//https://expressjs.com/en/guide/routing.html
+
+//Routing refers to determining how an application responds to a client request to a particular endpoint, which is a URI (or path) and a specific HTTP request method (GET, POST, and so on).
+//https://expressjs.com/en/starter/basic-routing.html
+
+//HTTP GET. a function handler for the home page.
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/index.html");//TODO: /public
 });
 
+//HTTP POST. chats.
 app.post("/messages", (req, res) => {
 	console.log(req.body);
 	console.log("server got a new message:" + req.body.message);
 
+	//run the SQL query with the param.
+	//https://github.com/TryGhost/node-sqlite3/wiki/API
 	db.run("INSERT INTO messages VALUES(?)", req.body.message);
 
 	res.status(201);
@@ -26,11 +57,15 @@ app.post("/messages", (req, res) => {
 	});
 });
 
+//HTTP GET. chats.
 app.get("/messages", (req, res) => {
 	console.log("client asks for messages.");
 
 	res.status(200);
 	msgs = []
+
+	//run the SQL query with the callback function.
+	//https://github.com/TryGhost/node-sqlite3/wiki/API
 	db.all("SELECT * FROM messages", (err, msg_rows) => {
 		msg_rows.forEach((msg) => {
 			console.log(msg);
@@ -41,6 +76,10 @@ app.get("/messages", (req, res) => {
 
 		console.log("print msgs:");
 		console.log(msgs);
+
+		//Sends a JSON response. This method sends a response (with the correct content-type) that is the parameter converted to a JSON string using JSON.stringify().
+		//The parameter can be any JSON type, including object, array, string, Boolean, number, or null, and you can also use it to convert other values to JSON.
+		//https://expressjs.com/en/4x/api.html#res.json
 		res.json({
 			"data":msgs
 		});
@@ -61,7 +100,14 @@ app.get("/messages", (req, res) => {
 	});
 });*/
 
+//TODO: socket.io on connection & disconnect events.
+
+
+//node.js http module.
+//https://nodejs.org/api/http.html#serverlisten
 http.listen(port, () => {
 	console.log(`socket.io server running on ${port}`);
 });
 
+
+//TODO: express graceful shutdown: close http server & db.
