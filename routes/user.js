2b843725b988811820e9ddfa87687e68c61412d1
bothemrun <b05902133@ntu.edu.tw>
2023-08-13 13:21:10 +0800

    refactor: restful api naming: register, login, logout

--- routes/user.js
+++ routes/user.js
@@ -5,19 +5,19 @@ const router = express.Router();
 //MVC: user controller
 const user_controller = new (require("../controllers/user_controller").User_Controller)();
 
-//HTTP POST. register.
+//register
 //router.post("/register", user_controller.register.bind(user_controller));
-router.post("/register",
+router.post("/users",
 	(...args) => user_controller.register(...args)
 );
 
-//HTTP POST. login
-router.post("/login",
+//login
+router.post("/logins",
 	(...args) => user_controller.login(...args)
 );
 
-//HTTP POST. logout
-router.post("/logout",
+//logout
+router.delete("/logins",
 	(...args) => user_controller.logout(...args)
 );
 
